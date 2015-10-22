06efcfd59dbc6017d3d94043b77d2efc39d40eb4
@hbk <nut.rainbowpuke@gmail.com>
2015-10-22 18:25:47 +0700

    Fix merge conflict.

--- app/src/main/java/xyz/rollingstone/MainActivity.java
+++ app/src/main/java/xyz/rollingstone/MainActivity.java
@@ -40,9 +40,10 @@ public class MainActivity extends ActionBarActivity {
     public static final String SERVER_PORT = "xyz.rollingstone.server.port";
     public static final String RES_POS = "xyz.rollingstone.resolution.pos";
     public static final String CONTROL_PORT = "xyz.rollingstone.control.port";
-    //res_pos 0 = 480p, 1 = 720p, 2 = 1080p
+    // res_pos 0 = 480p, 1 = 720p, 2 = 1080p
 
     public static final String TAG = "VULUME";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
